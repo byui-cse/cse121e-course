203acf4f92c63c452879d535da801b96d9c58f6e
yenrabbyui <yenrabbyui@gmail.com>
2020-10-21 13:38:50 -0600

    fixed typos

--- docs/week01/provided_solution/io_explore.erl
+++ docs/week01/provided_solution/io_explore.erl
@@ -1,15 +1,10 @@
 -module(io_explore).
--export([print_greeting_nice/0,print_greeting_strange/0, get_data/0]).
+-export([print_greeting_nice/0,print_greeting_strange/0]).
 
 print_greeting_nice()->
     {ok,Name} = io:read("Hello, what's your name? "),
     io:format("It is so nice to meet you ~s.~n",[Name]).
 
 print_greeting_strange()->
-    Name = io:get_line("Hello, what's your name? "),
+    {ok,Name} = io:read("Hello, what's your name? "),
     io:format("It is so nice to meet you ~w.~n",[Name]).
-
-get_data() ->
-        {ok, Term} = io:read("Enter a number: "),
-        io:format("The number you entered plus one is: ~w~n", 
-                  [Term + 1]).
