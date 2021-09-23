4bde4768ba0c6e7d02ad5c4db00c83dc4fc7a5d0
yenrabbyui <yenrabbyui@gmail.com>
2021-09-23 16:07:24 -0600

    changed read to get_line

--- docs/week01/provided_solution/io_explore.erl
+++ docs/week01/provided_solution/io_explore.erl
@@ -2,9 +2,9 @@
 -export([print_greeting_nice/0,print_greeting_strange/0]).
 
 print_greeting_nice()->
-    {ok,Name} = io:read("Hello, what's your name? "),
-    io:format("It is so nice to meet you ~s.~n",[Name]).
+    Name = io:get_line("Hello, what's your name? "),
+    io:format("It is so nice to meet you ~s~n",[Name]).
 
 print_greeting_strange()->
-    {ok,Name} = io:read("Hello, what's your name? "),
-    io:format("It is so nice to meet you ~w.~n",[Name]).
+    Name = io:get_line("Hello, what's your name? "),
+    io:format("It is so nice to meet you ~w~n",[Name]).
